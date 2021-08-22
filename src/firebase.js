54a8fd38880ffeb5ab2b2e51e9c0f0226ce8bc0c
sahil937 <meharsahil207@gmail.com>
2021-08-22 05:03:48 -0500

    Firebase is configed

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCbbrmoeBU_mZmdqxMoM8lNShTaSGbkaBM",
+    authDomain: "whatsapp-mern-e5480.firebaseapp.com",
+    projectId: "whatsapp-mern-e5480",
+    storageBucket: "whatsapp-mern-e5480.appspot.com",
+    messagingSenderId: "73129035282",
+    appId: "1:73129035282:web:1f2f3f08b732da7e7b3acb"
+  };
